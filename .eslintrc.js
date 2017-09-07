ceab9bb691d793144f9d7a7b0af8e5de056dbb7d
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-09-07 12:50:54 +0300

    simple gradient

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
         "semi": [
             "error",
             "always"
-        ]
+        ],
+        "no-console": ["error", { allow: ["warn", "error"] }]
     }
 };
--- src/Engine/GL.js
+++ src/Engine/GL.js
@@ -15,6 +15,7 @@ class GL {
 			program: shaderProgram,
 			attribLocations: {
 				vertexPosition: this.gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+				vertexColor: this.gl.getAttribLocation(shaderProgram, 'aVertexColor'),
 			},
 			uniformLocations: {
 				projectionMatrix: this.gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
@@ -31,19 +32,28 @@ class GL {
 	}
 
 	_initBuffers(gl) {
-		const positionBuffer = gl.createBuffer();		
-		gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-		
 		const positions = [
 			1.0,  1.0,
 			-1.0,  1.0,
 			1.0, -1.0,
 			-1.0, -1.0,
 		];
-		
+		const positionBuffer = gl.createBuffer();		
+		gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-		return {position: positionBuffer};
+		const colors = [
+			1.0,  1.0,  1.0,  1.0,    // white
+			1.0,  0.0,  0.0,  1.0,    // red
+			0.0,  1.0,  0.0,  1.0,    // green
+			0.0,  0.0,  1.0,  1.0,    // blue
+		];
+			
+		const colorBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+		return {position: positionBuffer, color: colorBuffer};
 	}
 
 	_initWebGL() {      
@@ -123,17 +133,15 @@ class GL {
 
 		glmatrix.mat4.translate(modelViewMatrix,     // destination matrix
 			modelViewMatrix,     // matrix to translate
-			[-0.0, 0.0, -9.0]);  // amount to translate
+			[-0.0, 0.0, -6.0]);  // amount to translate
 
-		// Tell WebGL how to pull out the positions from the position
-		// buffer into the vertexPosition attribute.
+		// Tell WebGL how to pull out the positions from the position buffer into the vertexPosition attribute
 		{
-			const numComponents = 2;  // pull out 2 values per iteration
-			const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-			const normalize = false;  // don't normalize
-			const stride = 0;         // how many bytes to get from one set of values to the next
-			// 0 = use type and numComponents above
-			const offset = 0;         // how many bytes inside the buffer to start from
+			const numComponents = 2;
+			const type = gl.FLOAT;
+			const normalize = false;
+			const stride = 0;
+			const offset = 0;
 			gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
 			gl.vertexAttribPointer(
 				programInfo.attribLocations.vertexPosition,
@@ -145,6 +153,25 @@ class GL {
 			gl.enableVertexAttribArray(
 				programInfo.attribLocations.vertexPosition);
 		}
+
+		{
+			const numComponents = 4;
+			const type = gl.FLOAT;
+			const normalize = false;
+			const stride = 0;
+			const offset = 0;
+			gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+			gl.vertexAttribPointer(
+				programInfo.attribLocations.vertexColor,
+				numComponents,
+				type,
+				normalize,
+				stride,
+				offset);
+			gl.enableVertexAttribArray(
+				programInfo.attribLocations.vertexColor);
+		}
+
 		// Tell WebGL to use our program when drawing
 		gl.useProgram(programInfo.program);
 
